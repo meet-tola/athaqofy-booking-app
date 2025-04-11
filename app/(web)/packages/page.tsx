5c39a082dc8548172e8e3034499d927bf65036c7
meet-tola <wasiusikiru7@gmail.com>
2025-04-11 19:05:42 +0100

    updating

--- app/(web)/packages/page.tsx
+++ app/(web)/packages/page.tsx
@@ -1,8 +1,9 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+import React, { Suspense, useState } from "react";
 import Breadcrumb from "@/components/BreadCrumb/BreadCrumb";
 import PageSearch from "@/components/PageSearch/PageSearch";
 import AllPackages from "@/components/AllPackages/AllPackages";
+import LoadingSpinner from "@/components/Loader/Loader";
 
 const PackagePage = () => {
   const breadcrumbItems = [{ href: "/", label: "Home" }, { label: "Packages" }];
@@ -11,9 +12,28 @@ const PackagePage = () => {
   return (
     <div className="mt-16 lg:mt-5">
       <Breadcrumb title="Packages" items={breadcrumbItems} />
-      <h1 className="text-3xl font-bold text-primary my-8 text-center font-roca">All Packages</h1>
-      <PageSearch setFetchKey={setFetchKey} />
-      <AllPackages key={fetchKey} />
+      <h1 className="text-3xl font-bold text-primary my-8 text-center font-roca">
+        All Packages
+      </h1>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center min-h-[400px]">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <PageSearch setFetchKey={setFetchKey} />
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center min-h-[400px]">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <AllPackages key={fetchKey} />
+      </Suspense>
     </div>
   );
 };
